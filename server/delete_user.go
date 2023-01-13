b2c6afce68b31d2a21fab9ed115577b323118a46
M. Rehan Abbasi <mrehanabbasi@yahoo.com>
2023-01-13 12:06:40 +0500

    fix case where no document with the given user ID is found

--- server/delete_user.go
+++ server/delete_user.go
@@ -6,7 +6,6 @@ import (
 
 	pb "github.com/mrehanabbasi/user-data-grpc/proto"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
@@ -15,12 +14,13 @@ import (
 func (*Server) DeleteUser(ctx context.Context, in *pb.UserId) (*emptypb.Empty, error) {
 	log.Println("Invoking DeleteUser function...")
 
-	_, err := collection.DeleteOne(ctx, bson.M{"id": in.Id})
+	res, err := collection.DeleteOne(ctx, bson.M{"id": in.Id})
 	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			return nil, status.Error(codes.NotFound, "User not found")
-		}
 		return nil, status.Errorf(codes.Internal, "Could not delete user: %v\n", err)
+	}
+
+	if res.DeletedCount == 0 {
+		return nil, status.Error(codes.NotFound, "User not found")
 	}
 
 	return &emptypb.Empty{}, nil
