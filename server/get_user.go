4e8ab1dc6e71ac6970fb05b1f5c77ea7e2b8d978
M. Rehan Abbasi <mrehanabbasi@yahoo.com>
2023-01-13 09:48:52 +0500

    add GetUser function in server

---
+++ server/get_user.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/mrehanabbasi/user-data-grpc/proto"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (*Server) GetUser(ctx context.Context, in *pb.UserId) (*pb.User, error) {
+	log.Println("Invoked GetUser function...")
+
+	user := pb.User{}
+
+	err := collection.FindOne(ctx, bson.M{"id": in.Id}).Decode(&user)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, status.Error(codes.NotFound, "User not found")
+		}
+		return nil, status.Errorf(codes.Internal, "Failed to get user: %v\n", err)
+	}
+
+	return &user, nil
+}
